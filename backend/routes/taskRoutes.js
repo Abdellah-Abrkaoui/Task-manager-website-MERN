b8d6906c3fe28f047689b4def6018a787b50e1b4
abdellah abrkaoui <abdellahabrkaoui14@gmail.com>
2025-05-12 19:41:43 +0100

    misc changes on task Controller & Route logic

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -21,9 +21,9 @@ router.get("/user-dashboard-data", protect, getUserDashboardData);
 router.get("/", protect, getTasks); // get all tasks (admin : all, User : assigned)
 router.get("/:id", protect, getTasksById); // get task by id
 router.post("/", protect, adminOnly, createTask); // create task (by admin only)
-router.put("/dashboard-data", protect, updateTask); // update task detail
-router.delete("/dashboard-data", protect, deleteTask); // delete a task (by admin only)
-router.put("/dashboard-data", protect, updateTaskStatus); // update task status
-router.put("/dashboard-data", protect, updateTaskChecklist); // update task checklist
+router.put("/:id", protect, updateTask); // update task detail
+router.delete("/:id", protect, deleteTask); // delete a task (by admin only)
+router.put("/:id/status", protect, updateTaskStatus); // update task status
+router.put("/:id/todo", protect, updateTaskChecklist); // update task checklist
 
 module.exports = router;
