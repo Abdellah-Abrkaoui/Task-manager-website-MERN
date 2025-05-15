ec96dd10b3fabb349918b3345867b3d4e8ea9915
abdellah abrkaoui <abdellahabrkaoui14@gmail.com>
2025-05-15 19:00:24 +0100

    added uploadImage logic

---
+++ frontend/src/utils/uploadImage.js
@@ -0,0 +1,27 @@
+import { API_PATH } from "./apiPaths";
+import axiosInstance from "./axiosInstance";
+
+const uploadImage = async (imageFile) => {
+  const formData = new FormData();
+
+  // Append image file to form data
+  formData.append("image", imageFile);
+
+  try {
+    const res = await axiosInstance.post(
+      API_PATH.IMAGE.UPLOAD_IMAGE,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return res.data;
+  } catch (error) {
+    console.error("Error uploading the image:", error);
+    throw error;
+  }
+};
+
+export default uploadImage;
